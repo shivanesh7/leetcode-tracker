e0552c9441a54a8f209f04ce374700a43f1425af
shivanesh7 <shrishivanesh007@gmail.com>
2026-07-09 09:46:51 +0530

    Create 1400-FindWinnerOnATicTacToeGame

---
+++ 1400-FindWinnerOnATicTacToeGame/1400-FindWinnerOnATicTacToeGame.java
@@ -0,0 +1,31 @@
+// Last updated: 7/9/2026, 9:46:51 AM
+class Solution {
+    public String tictactoe(int[][] moves) {
+        int[] rows = new int[3];
+        int[] cols = new int[3];
+        int diag = 0;
+        int anti = 0;
+
+        for (int i = 0; i < moves.length; i++) {
+            int r = moves[i][0];
+            int c = moves[i][1];
+
+            int player = (i % 2 == 0) ? 1 : -1;
+
+            rows[r] += player;
+            cols[c] += player;
+
+            if (r == c) diag += player;
+            if (r + c == 2) anti += player;
+
+            if (Math.abs(rows[r]) == 3 ||
+                Math.abs(cols[c]) == 3 ||
+                Math.abs(diag) == 3 ||
+                Math.abs(anti) == 3) {
+                return player == 1 ? "A" : "B";
+            }
+        }
+
+        return moves.length == 9 ? "Draw" : "Pending";
+    }
+}
