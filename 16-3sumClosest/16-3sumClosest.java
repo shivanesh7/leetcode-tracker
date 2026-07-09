dee7343da92ed0ced3a454bb6da2def3850b1586
shivanesh7 <shrishivanesh007@gmail.com>
2026-07-09 09:52:02 +0530

    Create 16-3sumClosest

---
+++ 16-3sumClosest/16-3sumClosest.java
@@ -0,0 +1,29 @@
+// Last updated: 7/9/2026, 9:52:01 AM
+import java.util.Arrays;
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                    closest = sum;
+                }
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return sum;
+                }
+            }
+        }
+        return closest;
+    }
+}
