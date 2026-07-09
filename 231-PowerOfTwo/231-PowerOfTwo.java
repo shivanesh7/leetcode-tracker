f25c88b14b1b2ec7e66ecaa1fe9ae8b005bfa5ec
shivanesh7 <shrishivanesh007@gmail.com>
2026-07-09 09:50:21 +0530

    Create 231-PowerOfTwo

---
+++ 231-PowerOfTwo/231-PowerOfTwo.java
@@ -0,0 +1,10 @@
+// Last updated: 7/9/2026, 9:50:20 AM
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        return n == 1;
+    }
+}
