8353bb278fc9e9ed6734bde424f0a0a89d15bee4
shivanesh7 <shrishivanesh007@gmail.com>
2026-07-09 09:47:15 +0530

    Create 1252-BreakAPalindrome

---
+++ 1252-BreakAPalindrome/1252-BreakAPalindrome.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 9:47:14 AM
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        int n = palindrome.length();
+        if (n == 1) return "";
+
+        char[] arr = palindrome.toCharArray();
+
+        for (int i = 0; i < n / 2; i++) {
+            if (arr[i] != 'a') {
+                arr[i] = 'a';
+                return new String(arr);
+            }
+        }
+
+        arr[n - 1] = 'b';
+        return new String(arr);
+    }
+}
